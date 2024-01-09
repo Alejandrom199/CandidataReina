e3e82def52326e8a9984351e84b484d8fc3b7d44
Saúl Maldonado lópez <106854444+Alejandrom199@users.noreply.github.com>
2024-01-09 09:37:09 -0500

    cambios de arreglos

--- CapaNegocio/Entidades/CN_Candidatas.cs
+++ CapaNegocio/Entidades/CN_Candidatas.cs
@@ -23,12 +23,8 @@ namespace CapaNegocio.Entidades
         private int id_Carrera;
         private byte[] imagenBytes;
 
-        private string titulo1, descripcion1, titulo2, descripcion2, titulo3, descripcion3, titulo4, descripcion4;
-        private byte[] foto1, foto2, foto3, foto4;
 
-
-        public CN_Candidatas(int id, string nombre, string apellidos, int edad, string telefono, string provincia, string pasatiempos, string habilidades, string intereses, string aspiraciones, int semestre, int id_carrera,
-            string titulo1, string descripcion1, string titulo2, string descripcion2, string titulo3, string descripcion3, string titulo4, string descripcion4)
+        public CN_Candidatas(int id, string nombre, string apellidos, int edad, string telefono, string provincia, string pasatiempos, string habilidades, string intereses, string aspiraciones, int semestre, int id_carrera)
         {
             this.id = id;
             this.nombre = nombre;
@@ -42,14 +38,6 @@ namespace CapaNegocio.Entidades
             this.aspiraciones = aspiraciones;
             this.semestre = semestre;
             this.id_Carrera = id_carrera;
-            this.titulo1 = titulo1;
-            this.descripcion1 = descripcion1;
-            this.titulo2 = titulo2;
-            this.descripcion2 = descripcion2;
-            this.titulo3 = titulo3;
-            this.descripcion3 = descripcion3;
-            this.titulo4 = titulo4;
-            this.descripcion4 = descripcion4;
         }
 
         public CN_Candidatas()
@@ -67,18 +55,6 @@ namespace CapaNegocio.Entidades
             semestre = -1;
             id_Carrera = -1;
             imagenBytes = null;
-            titulo1 = string.Empty;
-            descripcion1 = string.Empty;
-            foto1 = null;
-            titulo2 = string.Empty;
-            descripcion2 = string.Empty;
-            foto2 = null;
-            titulo3 = string.Empty;
-            descripcion3 = string.Empty;
-            foto3 = null;
-            titulo4 = string.Empty;
-            descripcion4 = string.Empty;
-            foto4 = null;
         }
         public int Id
         {
@@ -144,73 +120,13 @@ namespace CapaNegocio.Entidades
             get { return imagenBytes; } set { imagenBytes = value; }
         }
 
-        public string Titulo1
-        {
-            get { return titulo1; } set { titulo1 = value; }
-        }
-
-        public string Descripcion1
-        {
-            get { return descripcion1; } set { descripcion1 = value; }
-        }
-
-        public byte[] Foto1
-        {
-            get { return foto1; } set { foto1 = value; }
-        }
-
-        public string Titulo2
-        {
-            get { return titulo2; } set { titulo2 = value; }
-        }
-
-        public string Descripcion2
-        {
-            get { return descripcion2; } set { descripcion2 = value; }
-        }
-
-        public byte[] Foto2
-        {
-            get { return foto2; } set { foto2 = value; }
-        }
-
-        public string Titulo3
-        {
-            get { return titulo3; }  set { titulo3 = value; }
-        }
-
-        public string Descripcion3
-        {
-            get { return descripcion3; } set { descripcion3 = value; }
-        }
-
-        public byte[] Foto3
-        {
-            get { return foto3; } set { foto3 = value; }
-        }
-
-        public string Titulo4
-        {
-            get { return titulo4; } set { titulo4 = value; }
-        }
-
-        public string Descripcion4
-        {
-            get { return descripcion4; } set { descripcion4 = value; }
-        }
-
-        public byte[] Foto4
-        {
-            get { return foto4; } set { foto4 = value; }
-        }
-
         public DataTable GetListaCandidata()
         {
             Console.WriteLine("entro al lista Candidatas");
             try
             {
 
-                return obj_capa_datos.EjecutarSelect("SELECT * FROM tb_Candidata;");
+                return obj_capa_datos.EjecutarSelect("SELECT * FROM tb_candidata;");
             }
             catch (Exception e)
             {
@@ -223,7 +139,7 @@ namespace CapaNegocio.Entidades
             try
             {
                 // Modifica la consulta SQL para incluir el parámetro de imagen
-                var sql = "INSERT INTO tb_Candidata(nombre, apellidos, edad, telefono, provincia, pasatiempos, habilidades, intereses, aspiraciones, semestre, id_carrera, imagen, foto1, titulo1, descripcion1,  foto2, titulo2, descripcion2, foto3, titulo3, descripcion3 , foto4, titulo4, descripcion4)" +
+                var sql = "INSERT INTO tb_Candidata(nombre, apellidos, edad, telefono, provincia, pasatiempos, habilidades, intereses, aspiraciones, semestre, id_carrera, imagen)" +
                     "VALUES(" +
                     "'" + candidata.nombre + "'," +
                     "'" + candidata.apellidos + "'," +
@@ -236,28 +152,12 @@ namespace CapaNegocio.Entidades
                     "'" + candidata.aspiraciones + "'," +
                     "" + candidata.semestre + "," +
                     "" + candidata.id_Carrera + "," +
-                    "@imagen, @foto1, @titulo1, @descripcion1,  @foto2, @titulo2, @descripcion2, @foto3, @titulo3, @descripcion3 , @foto4, @titulo4, @descripcion4 )";
+                    "@imagen)";
 
                 // Agrega el parámetro de imagen a la consulta SQL
                 var parameters = new List<SqlParameter>
                 {
-                    new SqlParameter("@imagen", SqlDbType.VarBinary) { Value = candidata.imagenBytes },
-
-                    new SqlParameter("@foto1", SqlDbType.VarBinary) { Value = candidata.foto1 },
-                    new SqlParameter("@titulo1", SqlDbType.VarChar) { Value = candidata.titulo1 },
-                    new SqlParameter("@descripcion1", SqlDbType.VarChar) { Value = candidata.descripcion1 },
-
-                    new SqlParameter("@foto2", SqlDbType.VarBinary) { Value = candidata.foto2 },
-                    new SqlParameter("@titulo2", SqlDbType.VarChar) { Value = candidata.titulo2 },
-                    new SqlParameter("@descripcion2", SqlDbType.VarChar) { Value = candidata.descripcion2 },
-
-                    new SqlParameter("@foto3", SqlDbType.VarBinary) { Value = candidata.foto3 },
-                    new SqlParameter("@titulo3", SqlDbType.VarChar) { Value = candidata.titulo3 },
-                    new SqlParameter("@descripcion3", SqlDbType.VarChar) { Value = candidata.descripcion3 },
-
-                    new SqlParameter("@foto4", SqlDbType.VarBinary) { Value = candidata.foto4 },
-                    new SqlParameter("@titulo4", SqlDbType.VarChar) { Value = candidata.titulo4 },
-                    new SqlParameter("@descripcion4", SqlDbType.VarChar) { Value = candidata.descripcion4 },
+                    new SqlParameter("@imagen", SqlDbType.VarBinary) { Value = candidata.imagenBytes }
                 };
 
                 return obj_capa_datos.EjecutarSQL(sql, parameters);
