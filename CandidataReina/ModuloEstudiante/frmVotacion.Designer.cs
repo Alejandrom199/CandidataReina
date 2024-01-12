460a364d20a708730af28924a45c9a8d49384dd9
yunitop26 <domenica.bravoa@ug.edu.ec>
2024-01-12 14:06:01 -0500

    bbuton

--- CandidataReina/ModuloEstudiante/frmVotacion.Designer.cs
+++ CandidataReina/ModuloEstudiante/frmVotacion.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmVotacion";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(219, 74);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 75);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // frmVotacion
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "frmVotacion";
+            Text = "frmVotacion";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
